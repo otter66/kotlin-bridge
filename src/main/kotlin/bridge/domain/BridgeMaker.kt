6c4df081e261c40b966b8a3ebed222d5268a7591
otter <a33397397@gmail.com>
2022-12-10 23:22:50 +0900

    feat(domain): 다리 랜덤 발행 기능 작성

--- src/main/kotlin/bridge/domain/BridgeMaker.kt
+++ src/main/kotlin/bridge/domain/BridgeMaker.kt
@@ -1,6 +1,7 @@
 package bridge.domain
 
 import bridge.BridgeNumberGenerator
+import bridge.values.Movement
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -11,6 +12,15 @@ class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     fun makeBridge(size: Int): List<String> {
+        val bridge: MutableList<String> = mutableListOf()
+
+        repeat(size) {
+            when (bridgeNumberGenerator.generate()) {
+                Movement.UP.generateNumber -> bridge.add(Movement.UP.command)
+                Movement.DOWN.generateNumber -> bridge.add(Movement.DOWN.command)
+            }
+        }
+
         return listOf()
     }
 }
