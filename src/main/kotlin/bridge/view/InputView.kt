2f31b3375f415bf7c0e73b4618b4c7e91b10bb1c
otter <a33397397@gmail.com>
2022-12-10 22:43:59 +0900

    feat: 입출력 기능 작성

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,27 +1,44 @@
 package bridge.view
 
+import bridge.values.REQUIRE_BRIDGE_LENGTH_MESSAGE
+import bridge.values.REQUIRE_GAME_RETRY_COMMAND_MESSAGE
+import bridge.values.REQUIRE_MOVEMENT_MESSAGE
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 class InputView {
+
+    val output = OutputView()
+
     /**
      * 다리의 길이를 입력받는다.
      */
     fun readBridgeSize(): Int {
-        return 0
+        output.printMessage(REQUIRE_BRIDGE_LENGTH_MESSAGE)
+        val input = Console.readLine()
+
+        return input.toInt()
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        output.printMessage(REQUIRE_MOVEMENT_MESSAGE)
+        val input = Console.readLine()
+
+        return input
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        output.printMessage(REQUIRE_GAME_RETRY_COMMAND_MESSAGE)
+        val input = Console.readLine()
+
+        return input
     }
 }
